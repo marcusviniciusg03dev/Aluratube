311f689c04632d7d0d501620380926b61f22da87
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2022-11-16 07:15:16 -0300

    theme type correction

--- src/components/Theme.d.ts
+++ src/components/Theme.d.ts
@@ -3,12 +3,10 @@ import { ThemeModes } from '../contexts/ThemeModeContext';
 
 declare module 'styled-components' {
     export interface DefaultTheme {
-        [ThemeMode: ThemeModes]: {
-            backgroundBase: string
-            backgroundLevel1: string
-            backgroundLevel2: string
-            borderBase: string
-            textColorBase: string
-        }
+        backgroundBase: string
+        backgroundLevel1: string
+        backgroundLevel2: string
+        borderBase: string
+        textColorBase: string
     }
 }
